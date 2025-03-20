fecb2d97c29b4ef2492541f4fa0ca0c436c0a21e
JoseCarlos05 <jgomezbotana@safareyes.es>
2025-03-20 19:18:39 +0100

    Commit inicial Backend

---
+++ Backend_TFG/src/test/java/org/example/backend_tfg/BackendTfgApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.backend_tfg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTfgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
