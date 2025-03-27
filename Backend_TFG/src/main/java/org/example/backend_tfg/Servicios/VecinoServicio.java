c15d524dc42c7bebd2d9d83958db1ac4f8e699dd
Ricardo <rtovarfernandez@safareyes.es>
2025-03-27 11:34:16 +0100

    Ver vecino por id y ver vecino por idUsuario

--- Backend_TFG/src/main/java/org/example/backend_tfg/Servicios/VecinoServicio.java
+++ Backend_TFG/src/main/java/org/example/backend_tfg/Servicios/VecinoServicio.java
@@ -1,6 +1,9 @@
 package org.example.backend_tfg.Servicios;
 
 import lombok.AllArgsConstructor;
+import org.example.backend_tfg.DTOs.RegistrarVecinoDTO;
+import org.example.backend_tfg.DTOs.VecinoDTO;
+import org.example.backend_tfg.Modelos.Vecino;
 import org.example.backend_tfg.Repositorios.IVecinoRepositorio;
 import org.springframework.stereotype.Service;
 
@@ -10,4 +13,30 @@ public class VecinoServicio {
 
     private IVecinoRepositorio iVecinoRepositorio;
 
+    public VecinoDTO buscarVecinoID(Integer id){
+        Vecino vecino = iVecinoRepositorio.findById(id)
+                .orElseThrow(() -> new RuntimeException("No existe un vecino con este ID."));
+
+        return getVecinoDTO(vecino);
+    }
+
+    public VecinoDTO busccarClienteUsuarioID(Integer idUsuario) {
+        Vecino vecino = iVecinoRepositorio.findByUsuario_Id(idUsuario);
+        return getVecinoDTO(vecino);
+    }
+
+    public static VecinoDTO getVecinoDTO(Vecino vecino){
+        VecinoDTO dtoNuevo  = new VecinoDTO();
+
+        dtoNuevo.setId(vecino.getId());
+        dtoNuevo.setNombre(vecino.getNombre());
+        dtoNuevo.setApellidos(vecino.getApellidos());
+        dtoNuevo.setDNI(vecino.getDNI());
+        dtoNuevo.setTelefono(vecino.getTelefono());
+        dtoNuevo.setDireccionPersonal(vecino.getDireccionPersonal());
+        dtoNuevo.setFechaNacimiento(vecino.getFechaNacimiento());
+        dtoNuevo.setNumeroCuenta(vecino.getNumCuenta());
+
+        return dtoNuevo;
+    }
 }
