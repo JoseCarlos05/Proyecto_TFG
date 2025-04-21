16f361c1fcbb37903f5c30d5ede3bd261390580d
CARLOSFDEZIII <118250046+CARLOSFDEZIII@users.noreply.github.com>
2025-04-21 13:08:52 +0200

    Commmit Configurar Perfil

--- Frontend_TFG/src/app/perfil/perfil.component.ts
+++ Frontend_TFG/src/app/perfil/perfil.component.ts
@@ -3,15 +3,17 @@ import {IonicModule} from "@ionic/angular";
 import {Router} from "@angular/router";
 import {CommonModule} from "@angular/common";
 import {FooterComponent} from "../footer/footer.component";
+import {HeaderComponent} from "../header/header.component";
 
 @Component({
     selector: 'app-perfil',
     templateUrl: './perfil.component.html',
     styleUrls: ['./perfil.component.scss'],
-  imports: [IonicModule, CommonModule, FooterComponent],
+  imports: [IonicModule, CommonModule, FooterComponent, HeaderComponent],
     standalone: true,
 })
 export class PerfilComponent  implements OnInit {
+  formLogin: any;
 
   constructor(private router: Router) { }
 
