d801097c461ff06d69626bf6f4d8ea110882e935
CARLOSFDEZIII <118250046+CARLOSFDEZIII@users.noreply.github.com>
2025-06-02 12:45:27 +0200

    Commit component Home

--- Frontend_TFG/src/app/home/home.page.ts
+++ Frontend_TFG/src/app/home/home.page.ts
@@ -1,15 +1,31 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import {IonicModule} from "@ionic/angular";
-import {CommonModule} from "@angular/common";
+import {Component, OnInit} from '@angular/core';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonRow,
+  IonGrid,
+  IonCol,
+  IonImg,
+  IonButton
+} from '@ionic/angular/standalone';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonicModule, CommonModule],
+  imports: [IonContent, IonRow, IonGrid, IonCol, IonImg, IonButton],
   standalone: true
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {}
+
+  accionBoton() {
+    this.router.navigate(['/inicio-sesion']);
+  }
 }
